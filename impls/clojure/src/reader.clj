14c203a83e6c1c56a7ce0b73850b6c6fdbba82dc
Eric L <ercliou+github@gmail.com>
2022-09-25 01:31:01 +0100

    second impl of read form

--- impls/clojure/src/reader.clj
+++ impls/clojure/src/reader.clj
@@ -21,77 +21,81 @@
 
 (declare read-form)
 
-(defn read-token [token]
+(defn read-token* [token]
   (cond
     (re-matches #"-?\d+" token) (Long/parseLong token)
     (re-matches #"-?\d+(\.\d+)?" token) (Double/parseDouble token)
     :else (throw (ex-info "Reader can't read token. Unknown format." {:token token}))))
 
-(comment
-      (re-matches #"-?\d+" "+11")
-      (clojure.edn/read-string "+11")
-      (Float/parseFloat "-11")
-)
-
-(comment
-  "(+ 1 (- 3 2 ) )"
-  ;
-  
-  (rest [1 2 3])
-  )
-
-(defn tap [x] (prn x) x)
+(defn read-token [[head & remaining]]
+  {:result (read-token* head)
+   :tokens remaining})
 
 (defn read-list [tokens]
   (loop [tokens (rest tokens) ; pop off open paren
          result []]
-    (prn "loop tokens" tokens)
-    (prn "loop result" result)
-    (let [token (first tokens)]
+    (let [[head & remaining] tokens]
       (cond
-        (not= ")" token) (let [read-result (read-form tokens)]
-                           (prn "read-result" read-result)
-                           (recur (:tokens read-result)
-                                  (conj result (:result read-result))))
-        (= ")" token) {:result result
-                       :tokens (rest tokens)}
+        (not= ")" head) (let [form (read-form tokens)]
+                          (recur (:tokens form)
+                                 (conj result (:result form))))
+        (= ")" head) {:result result
+                      :tokens remaining}
 
         ; else unbalanced if close-paren not found
-
-        )))
-
-  )
+        ))))
 
 (defn read-form [tokens]
-  (prn "read-form" tokens)
   (let [token (first tokens)]
     (cond
       (= "(" token) (read-list tokens)
-      :else (tap {:result (read-token token)
-                  :tokens (rest tokens)})
-      )
-    ))
 
+      ; simplification: in case of loose tokens to evaluate, read the first token only, ignore the rest
+      :else (read-token tokens))))
+
+(defn read-form2
+  "Simpler but less fancy.
+  Use a explicit stack, instead of the call stack, to track the tree depth."
+  [tokens]
+  (reduce (fn [stack t]
+            (cond
+              (= t "(") (conj stack [])
+              (= t ")") (let [current (peek stack) ; pop top stack and merge into parent
+                              remaining (pop stack)
+                              parent (peek remaining)
+                              new-parent (conj parent current)]
+                          (conj (pop remaining) new-parent))
+              :else (let [token (read-token* t)]
+                      (if (peek stack)
+                        (conj (pop stack) (conj (peek stack) token)) ; update top stack with new token
+
+                        ; simplification: in case of loose tokens to evaluate, read the first token only, ignore the rest
+                        (conj stack token)))))
+          tokens))
+
+
+(comment (read-form2 ["-2.1"])
+         ; => -2.1
+         )
 (comment (read-form ["-2.1"])
          ; => -2.1
          )
 (comment (read-form ["(" ")"])
          ; => ( 1 2 )
          )
-(comment (read-form ["(" "1" "2" ")"])
+(comment (read-form2 ["(" "1" "2" ")"])
          ; => ( 1 2 )
          )
-(comment (read-form ["(" "0" "(" "1" "4" ")" "2" "3" ")"])
+(comment (read-form2 ["(" "0" "(" "1" "4" ")" "2" "3" ")"])
          ; => ( 1 2 )
          )
-(comment *e)
 
 (defn read-str [s]
   (let [tokens (tokenize s)]
     (read-form tokens)))
 
+(comment (read-str "(0 (1 2) 3 4 (5) 6)"))
+
 (comment (tokenize " ( 123 \"abc\" ) ")
          (tokenize "123"))
-
-(comment (type 2.2))
 
